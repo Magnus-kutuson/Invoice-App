021af6b9ed69d424e3f0daf3bae1c4b06a586254
Magnus <bcict20070@ttu.edu.gh>
2025-01-29 09:35:03 +0000

    add layout and sidebar components to app; update global styles and responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './component/layout/layout.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
