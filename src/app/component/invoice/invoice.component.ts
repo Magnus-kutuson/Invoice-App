2d6e0ec7e8c108f29acee42d03f1605897bac389
Magnus <bcict20070@ttu.edu.gh>
2025-01-29 15:24:05 +0000

    add data service and invoice component; integrate HTTP client and display invoice data

--- src/app/component/invoice/invoice.component.ts
+++ src/app/component/invoice/invoice.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { DataServiceService } from '../shared/data-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
 export class InvoiceComponent {
 
+  data: any;
+
+  constructor(private dataService: DataServiceService) {
+    this.dataService.getJSON().subscribe(data => {
+      this.data = data;
+    });
+  }
 }
