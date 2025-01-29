7c04ea0a47eee8b8943d3c3ba8f57026cfd8fa87
Magnus <bcict20070@ttu.edu.gh>
2025-01-29 13:48:28 +0000

    add header component with layout updates; change sidebar background color and improve styles

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ThemeChangerComponent } from '../theme-changer/theme-changer.component';
 import { AvatarComponent } from '../avatar/avatar.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ThemeChangerComponent, AvatarComponent, RouterOutlet],
+  imports: [ThemeChangerComponent, AvatarComponent, RouterOutlet, HeaderComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
