7990660c77bac7185d5803fd702ee990415020a1
Magnus <bcict20070@ttu.edu.gh>
2025-01-29 14:47:57 +0000

    add invoice component and integrate it into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './component/layout/layout.component';
 import { HeaderComponent } from './component/header/header.component';
+import { InvoiceComponent } from './component/invoice/invoice.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent, HeaderComponent],
+  imports: [RouterOutlet, LayoutComponent, HeaderComponent, InvoiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
